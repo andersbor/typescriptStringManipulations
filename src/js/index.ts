b863d268396c07559c69230348404ce9d2a5ffbb
Anders Børjesson <anbo@zealand.dk>
2020-03-14 09:35:59 +0100

    more functions

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,22 @@
-interface Person {
-    firstName: string;
-    lastName: string;
-}
+let inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById("inputField");
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+let operationElement: HTMLSelectElement = <HTMLSelectElement>document.getElementById("operation");
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+let doItButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById("doItButton");
+doItButton.addEventListener("click", doIt);
+
+let resultElement: HTMLDivElement = <HTMLDivElement> document.getElementById("result");
+
+function doIt(): void {
+    let str : string = inputElement.value;
+    let op : string = operationElement.value;
+    let result: string = "nothing";
+    switch (op) {
+        case "lower": result = str.toLowerCase(); break;
+        case "upper": result = str.toUpperCase(); break; 
+        case "spacing": result = str.split("").join(" "); break; // extra
+        case "reverse": result = str.split("").reverse().join(""); break
+        // https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
+    }
+    resultElement.innerHTML = result;
+}
